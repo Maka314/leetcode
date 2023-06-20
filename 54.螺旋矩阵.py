13efd63c15929e56bb167e17faacd24f92c0d937
Mingchen <traveller31415@gmail.com>
2023-06-20 06:25:55 +0000

    54第二思路

--- 54.螺旋矩阵.py
+++ 54.螺旋矩阵.py
@@ -7,10 +7,26 @@
 # @lc code=start
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        import math
+        res = []
         xcount,ycount = len(matrix[0]),len(matrix)
-        res = [None for i in range(xcount*ycount)]
-        for iy,jy in enumerate(matrix):
-            for ix,jx in enumerate(jy):
-                res[iy*xcount+ix] = jx
+        round = math.ceil(min(xcount,ycount)/2)
+        for r in range(round):
+            #第一轮向右
+            y = r
+            for x in range(r,xcount-r):
+                res.append(matrix[y][x])
+            #第二轮向下
+            x = xcount-1-r
+            for y in range(r+1,ycount-r):
+                res.append(matrix[y][x])
+            #第三轮向左
+            y = ycount-1-r
+            for x in range(xcount-1-r,r-1,-1):
+                res.append(matrix[y][x])
+            #第四轮向上
+            x = r
+            for y in range(ycount-1-r,r,-1):
+                res.append(matrix[y][x])
         return res
 # @lc code=end
