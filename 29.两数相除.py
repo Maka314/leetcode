e46f614a88d8bcf340f2474f789aeec5918653ad
Mingchen <traveller31415@gmail.com>
2023-06-27 03:07:36 +0000

    两数相除修改

--- 29.两数相除.py
+++ 29.两数相除.py
@@ -7,23 +7,14 @@
 # @lc code=start
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if dividend == -2147483648:
+        if dividend == -2147483648 and divisor == -1:
             return 2147483647
-        mark = True
-        res = 0
-        if dividend<0:
-            dividend = -dividend
-            mark = not mark
-        if divisor<0:
-            divisor = -divisor
-            mark = not mark
-        while dividend>0:
-            dividend-=divisor
-            res+=1
-        if dividend<0:
-            res-=1
-        if not mark:
-            res = -res
-        return res
+        a, b, res = abs(dividend), abs(divisor), 0
+        for i in range(31, -1, -1):
+            # 2^i * b <= a 换句话说 a/b = 2^i + (a-2^i*b)/b
+            if (b << i) <= a:
+                res += 1 << i
+                a -= b << i
+        return res if (dividend > 0) == (divisor > 0) else -res
 # @lc code=end
 
