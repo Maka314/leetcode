f488331912e7867ffa2243da607d37074e0b3301
Mingchen <traveller31415@gmail.com>
2023-06-27 06:12:44 +0000

    第二错误思路

--- 31.下一个排列.py
+++ 31.下一个排列.py
@@ -12,8 +12,15 @@ class Solution:
         """
         max_i = len(nums)-1
         flag = True
+        v = nums[-1]
         for i,j in enumerate(nums[::-1]):
-            if i<max_i and nums[max_i-i-1]<j:
+            if i<max_i and nums[max_i-i-1]<v:
+                for c in range(max_i,max_i-i-1,-1):
+                    nums[c]=nums[c-1]
+                nums[max_i-i-1]=v
+                flag = False
+                break
+            elif nums[max_i-i-1]<j:
                 nums[max_i-i] = nums[max_i-i-1]
                 nums[max_i-i-1] = j
                 flag = False
@@ -24,6 +31,5 @@ class Solution:
                 nums[-(i+1)],nums[i] = j ,nums[-(i+1)]
                 if i+1>l/2:
                     break
-                
 # @lc code=end
 
