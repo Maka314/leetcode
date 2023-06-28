2fd1c10fc4f4ed276a43be97a37461b23097e3b7
mingchen <traveller31415@gmail.com>
2023-06-28 20:12:24 +0800

    测试提交

---
+++ 43.字符串相乘.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=43 lang=python3
+#
+# [43] 字符串相乘
+#
+
+# @lc code=start
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        for i in num1:
+            pass
+# @lc code=end
+
