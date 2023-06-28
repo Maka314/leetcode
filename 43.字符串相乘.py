dd8b00b28acac04d7c2c441cfb029e0addf8de86
mingchen <traveller31415@gmail.com>
2023-06-28 22:57:37 +0800

    怎么看都很稳

--- 43.字符串相乘.py
+++ 43.字符串相乘.py
@@ -7,7 +7,12 @@
 # @lc code=start
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        for i in num1:
-            pass
+        n1,n2 = 0,0
+        for i1,j1 in enumerate(num1[::-1]):
+            n1+=int(j1)*10**i1
+        for i2,j2 in enumerate(num2[::-1]):
+            n2+=int(j2)*10**i2
+        res = n1*n2
+        return str(res)
 # @lc code=end
 
