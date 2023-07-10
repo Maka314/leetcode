3a29733ce54955e35351571d0985ff2042f523a7
mingchen <traveller31415@gmail.com>
2023-07-10 23:50:29 +0800

    206，有点浮躁浪费了很多时间

---
+++ 206.反转链表.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=206 lang=python3
+#
+# [206] 反转链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head != None:
+            current = head
+            head = head.next
+            current.next = prev
+            prev = current
+        return prev
+# @lc code=end
+
