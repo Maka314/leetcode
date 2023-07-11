518e457e826a6b4f5b8394cf0221a11902941107
mingchen <traveller31415@gmail.com>
2023-07-11 22:36:39 +0800

    142解决

---
+++ 142.环形链表-ii.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=142 lang=python3
+#
+# [142] 环形链表 II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        s = set()
+        while head:
+            if head in s:
+                return head
+            s.add(head)
+            head = head.next
+        return None
+# @lc code=end
+
