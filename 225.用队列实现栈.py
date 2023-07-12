f6f0151c36bbedd05466726c646c42a038b1b435
mingchen <traveller31415@gmail.com>
2023-07-12 16:50:09 +0800

    堆栈&队列初探

---
+++ 225.用队列实现栈.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=225 lang=python3
+#
+# [225] 用队列实现栈
+#
+
+# @lc code=start
+class MyStack:
+
+    def __init__(self):
+        
+
+    def push(self, x: int) -> None:
+
+
+    def pop(self) -> int:
+
+
+    def top(self) -> int:
+
+
+    def empty(self) -> bool:
+
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+# @lc code=end
+
