9e20cf71fd67003db98e63d374b0bae3e316d517
mingchen <traveller31415@gmail.com>
2023-07-12 14:33:38 +0800

    更优解

--- 454.四数相加-ii.py
+++ 454.四数相加-ii.py
@@ -7,33 +7,24 @@
 # @lc code=start
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        resList1, resList2, resList3, resList4 = {},{},{},{}
-        for i,j in enumerate(nums1):
-            if j not in resList1:
-                resList1[j] = 1
-            else:
-                resList1[j] += 1
-        for i,j in enumerate(nums2):
-            for res in resList1:
-                if (res + j) not in resList2:
-                    resList2[res + j] = resList1[res]
+        abMap = {}
+        cdMap = {}
+        for i1 in nums1:
+            for i2 in nums2:
+                if i1 + i2 not in abMap:
+                    abMap[i1 + i2] = 1
                 else:
-                    resList2[res + j] += resList1[res]
-        for j in nums3:
-            for res in resList2:
-                if res + j not in resList3:
-                    resList3[res + j] = resList2[res]
+                    abMap[i1 + i2] += 1
+        for i3 in nums3:
+            for i4 in nums4:
+                if i3 + i4 not in cdMap:
+                    cdMap[i3 + i4] = 1
                 else:
-                    resList3[res + j] += resList2[res]
-        for j in nums4:
-            for res in resList3:
-                if res + j not in resList4:
-                    resList4[res + j] = resList3[res]
-                else:
-                    resList4[res + j] += resList3[res]
-        if 0 in resList4:
-            return resList4[0]
-        else:
-            return 0
+                    cdMap[i3 + i4] += 1
+        res = 0
+        for abSum in abMap:
+            if -abSum in cdMap:
+                res += abMap[abSum] * cdMap[-abSum]
+        return res
 # @lc code=end
 
