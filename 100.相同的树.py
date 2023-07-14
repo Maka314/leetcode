bdacb02dfc361b09f53da507b35dc85bc59758db
mingchen <traveller31415@gmail.com>
2023-07-14 09:46:12 +0800

    572错误解

---
+++ 100.相同的树.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=100 lang=python3
+#
+# [100] 相同的树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+# @lc code=end
+
---
+++ 572.另一棵树的子树.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode.cn id=572 lang=python3
+#
+# [572] 另一棵树的子树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def juger(p,q):
+            if not p and not q:
+                return True
+            elif not p or not q:
+                return False
+            elif p.val != q.val:
+                return False
+            else:
+                return juger(p.left, q.left) and juger(p.right, q.right)
+        
+        def find(root,subRoot):
+            if juger(root, subRoot):
+                return True
+            else:
+                res = juger(root.right, subRoot) or juger(root.left, subRoot)
+                return res
+        
+        return find(root,subRoot)
+# @lc code=end
+
