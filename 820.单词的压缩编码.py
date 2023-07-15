502a94bf547cab32ac5f25802f30bb48689d9fd3
mingchen <traveller31415@gmail.com>
2023-07-15 15:40:05 +0800

    一个具有多重子节点的树（未完成）

---
+++ 820.单词的压缩编码.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=820 lang=python3
+#
+# [820] 单词的压缩编码
+#
+
+# @lc code=start
+class treeNode:
+    def __init__(self,val) -> None:
+        self.val
+        self.next = []
+
+class compressTree:
+    def __init__(self) -> None:
+        self.root = treeNode(0)
+
+    def addWord(self,word):
+        temp
+
+
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+# @lc code=end
+
