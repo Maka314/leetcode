609232b673ded1cf52b6f7240782109e8cf27ffa
mingchen <traveller31415@gmail.com>
2023-07-15 15:44:57 +0800

    820 非树解法

---
+++ 820.单词的压缩编码.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=820 lang=python3
+#
+# [820] 单词的压缩编码
+#
+
+# @lc code=start
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        
+# @lc code=end
+
