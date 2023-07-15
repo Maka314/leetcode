0512bb4659e5baf91d66e86b5e4a45bd0e1196d3
mingchen <traveller31415@gmail.com>
2023-07-15 16:11:50 +0800

    初步完成多节点解，最后计数存在问题

--- 820.单词的压缩编码.py
+++ 820.单词的压缩编码.py
@@ -4,21 +4,55 @@
 # [820] 单词的压缩编码
 #
 
+
 # @lc code=start
 class treeNode:
-    def __init__(self,val) -> None:
-        self.val
-        self.next = []
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = {}
+
+    def addNode(self, node):
+        self.next[node.val] = node
+
+    def findNode(self, val):
+        if val in self.next:
+            return self.next[val]
+        else:
+            return None
+
 
 class compressTree:
     def __init__(self) -> None:
         self.root = treeNode(0)
 
-    def addWord(self,word):
-        temp
+    def addWord(self, word):
+        operator = self.root
+        for i in word[::-1]:
+            nextNode = operator.findNode(i)
+            if nextNode:
+                operator = nextNode
+            else:
+                nextNode = treeNode(i)
+                operator.addNode(nextNode)
+                operator = nextNode
+
+    def countTree(self, node: treeNode, layer=0):
+        res = 0
+        if not node.next:
+            return 1
+        else:
+            res = len(node.next)
+            for e in node.next:
+                res += self.countTree(node.next[e])
+        return res
 
 
 class Solution:
-    def minimumLengthEncoding(self, words: List[str]) -> int:
-# @lc code=end
+    def minimumLengthEncoding(self, words) -> int:
+        res = compressTree()
+        for w in words:
+            res.addWord(w)
+        print(res.countTree(res.root))
 
+
+# @lc code=end
