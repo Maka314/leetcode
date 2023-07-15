6aefddcd17c9228fe5ab4c1013c242be93fc6e2a
mingchen <traveller31415@gmail.com>
2023-07-15 14:48:54 +0800

    804

--- 804.唯一摩尔斯密码词.py
+++ 804.唯一摩尔斯密码词.py
@@ -4,8 +4,45 @@
 # [804] 唯一摩尔斯密码词
 #
 
+
 # @lc code=start
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-# @lc code=end
+        morse = [
+            ".-",
+            "-...",
+            "-.-.",
+            "-..",
+            ".",
+            "..-.",
+            "--.",
+            "....",
+            "..",
+            ".---",
+            "-.-",
+            ".-..",
+            "--",
+            "-.",
+            "---",
+            ".--.",
+            "--.-",
+            ".-.",
+            "...",
+            "-",
+            "..-",
+            "...-",
+            ".--",
+            "-..-",
+            "-.--",
+            "--..",
+        ]
+        res = set()
+        for w in words:
+            code = ""
+            for c in w:
+                code += morse[ord(c) - 97]
+            res.add(code)
+        return len(res)
 
+
+# @lc code=end
