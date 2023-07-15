098dcaaedc1ffe3b4c277367e4ff523121a27970
mingchen <traveller31415@gmail.com>
2023-07-15 14:39:53 +0800

    900题完成，但是看不出和树的关系

---
+++ 804.唯一摩尔斯密码词.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=804 lang=python3
+#
+# [804] 唯一摩尔斯密码词
+#
+
+# @lc code=start
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+# @lc code=end
+
--- 900.rle-迭代器.py
+++ 900.rle-迭代器.py
@@ -8,11 +8,23 @@
 class RLEIterator:
 
     def __init__(self, encoding: List[int]):
-
+        self.encoding = encoding
 
     def next(self, n: int) -> int:
-
-
+        res = -1
+        while self.encoding and n > 0:
+            if n == self.encoding[0]:
+                res = self.encoding[1]
+                self.encoding.pop(0)
+                self.encoding.pop(0)
+            elif n > self.encoding[0]:
+                n -= self.encoding[0]
+                self.encoding.pop(0)
+                self.encoding.pop(0)
+            else:
+                self.encoding[0] -= n
+                res = self.encoding[1]
+        return res
 
 # Your RLEIterator object will be instantiated and called as such:
 # obj = RLEIterator(encoding)
