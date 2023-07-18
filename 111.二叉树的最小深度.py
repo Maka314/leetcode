ca7ecc885efffa303a2109aaf836eb6a5150cba3
mingchen <traveller31415@gmail.com>
2023-07-18 20:59:02 +0800

    题意理解错误

---
+++ 111.二叉树的最小深度.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=111 lang=python3
+#
+# [111] 二叉树的最小深度
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        def pathFinder(root,path):
+            if not root:
+                return path
+            return min(pathFinder(root.left, path + 1), pathFinder(root.right, path + 1))
+        return pathFinder(root,0)
+# @lc code=end
+
