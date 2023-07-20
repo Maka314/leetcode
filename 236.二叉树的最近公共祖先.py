9aed949c1d06f486d8d538fa188efe65323c563b
mingchen <traveller31415@gmail.com>
2023-07-20 14:43:25 +0800

    更新笔记

--- 236.二叉树的最近公共祖先.py
+++ 236.二叉树的最近公共祖先.py
@@ -14,6 +14,9 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # 本题创新在于递归解可以返回不只一种数据结构，
+        # 这一题就可以返回节点或者None
+        # 不过其实还是对题意理解有问题
         if root == p or root == q or not root:
             return root
         
