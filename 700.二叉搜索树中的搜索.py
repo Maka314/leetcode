8b2b4482ad142a828093809733d78090e294a4e2
mingchen <traveller31415@gmail.com>
2023-07-20 11:48:53 +0800

    二叉树递归搜索

---
+++ 700.二叉搜索树中的搜索.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=700 lang=python3
+#
+# [700] 二叉搜索树中的搜索
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        leftRes = self.searchBST(root.left, val)
+        if leftRes:
+            return leftRes
+        rightRes = self.searchBST(root.right, val)
+        if rightRes:
+            return rightRes
+        return None
+
+# @lc code=end
+
