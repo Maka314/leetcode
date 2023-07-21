b8571a730254cb7aa2750dd957b90e48ae6edac8
mingchen <traveller31415@gmail.com>
2023-07-21 13:42:17 +0800

    优化算法

--- 77.组合.py
+++ 77.组合.py
@@ -8,20 +8,17 @@
 # @lc code=start
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        useable = [i for i in range(1, n + 1)]
-        return self.backTrack(useable, k)
-
-    def backTrack(self, useable, k):
-        if k == 1:
-            res = [[i] for i in useable]
-            return res
-        else:
-            res = []
-            for i in range(len(useable) - k + 1):
-                tempR = self.backTrack(useable[i + 1 :], k - 1)
-                for r in tempR:
-                    res += [[useable[i]] + r]
-            return res
+        result = []  # 存放结果集
+        self.backtracking(n, k, 1, [], result)
+        return result
+    def backtracking(self, n, k, startIndex, path, result):
+        if len(path) == k:
+            result.append(path[:])
+            return
+        for i in range(startIndex, n - (k - len(path)) + 2):  # 优化的地方
+            path.append(i)  # 处理节点
+            self.backtracking(n, k, i + 1, path, result)
+            path.pop()  # 回溯，撤销处理的节点
 
 
 # @lc code=end
