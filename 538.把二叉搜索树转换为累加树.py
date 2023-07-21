953e39e7f2adfc4e73e6b3fe50eab95c47bc1e21
mingchen <traveller31415@gmail.com>
2023-07-21 10:36:08 +0800

    随想录解

---
+++ 538.把二叉搜索树转换为累加树.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=538 lang=python3
+#
+# [538] 把二叉搜索树转换为累加树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        self.pre = 0  # 记录前一个节点的数值
+        self.traversal(root)
+        return root
+    def traversal(self, cur):
+        if cur is None:
+            return        
+        self.traversal(cur.right)
+        cur.val += self.pre
+        self.pre = cur.val
+        self.traversal(cur.left)
+# @lc code=end
+
