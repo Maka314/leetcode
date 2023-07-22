474f0ed4ae846d9c9b681926d1b34423d4e47b04
mingchen <traveller31415@gmail.com>
2023-07-22 11:01:01 +0800

    回溯法难度提升题

---
+++ 17.电话号码的字母组合.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=17 lang=python3
+#
+# [17] 电话号码的字母组合
+#
+
+# @lc code=start
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        self.res = []
+        self.keyboard = {
+            '2':['a','b','c'],
+            '3':['d','e','f'],
+            '4':['g','h','i'],
+            '5':['j','k','l'],
+            '6':['m','n','o'],
+            '7':['p','q','r','s'],
+            '8':['t','u','v'],
+            '9':['w','x','y','z']
+            }
+    
+    def backTracking(self,digits,track):
+        if not digits:
+            self.res.append(track)
+            return
+# @lc code=end
+
--- 40.组合总和-ii.py
+++ 40.组合总和-ii.py
@@ -4,25 +4,32 @@
 # [40] 组合总和 II
 #
 
+
 # @lc code=start
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def assimble(candidates, used, target):
-            res = []
-            print()
-            for i,j in enumerate(candidates):
-                if j<target:
-                    res+=assimble(candidates[:i] + candidates[i+1:],used+[j],target-j)
-                elif j==target:
-                    temp = used+[j]
-                    temp.sort()
-                    res+=[temp]
-            return res
-        tempRes = assimble(candidates,[],target)
-        res = []
-        for r in tempRes:
-            if r not in res:
-                res.append(r)
-        return res
-# @lc code=end
+        self.res = []
+        candidates.sort()
+        self.trackBacking(candidates, 0, [], 0, target)
+        return self.res
 
+    def trackBacking(self, candidates, startIndex, track, trackSum, target):
+        if trackSum == target:
+            self.res.append(track[:])
+            return
+
+        for i in range(startIndex, len(candidates)):
+            if i > startIndex and candidates[i] == candidates[i-1]:
+                continue #同样关键的优化
+                
+            if trackSum + candidates[i] > target:
+                break #关键优化
+
+            track.append(candidates[i])
+            trackSum += candidates[i]
+            self.trackBacking(candidates, i + 1, track, trackSum, target)
+            trackSum -= candidates[i]
+            track.pop()
+
+
+# @lc code=end
