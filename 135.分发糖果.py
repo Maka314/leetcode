aa840dd6d9917a9aad4a2d1537a03e541a80b74a
mingchen <traveller31415@gmail.com>
2023-07-24 13:47:54 +0800

    两遍循环贪心算法

--- 135.分发糖果.py
+++ 135.分发糖果.py
@@ -4,27 +4,20 @@
 # [135] 分发糖果
 #
 
+
 # @lc code=start
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        res = 1
-        candyForYou = 1
+        candyBox = [1 for _ in range(len(ratings))]
         for i in range(1, len(ratings)):
             if ratings[i] > ratings[i - 1]:
-                candyForYou += 1
-                res += candyForYou
-            elif ratings[i] < ratings[i - 1]:
-                candyForYou -= 1
-                if candyForYou:
-                    res += candyForYou
-                else:
-                    candyForYou = 1
-                    res += i + 1
-            else:
-                candyForYou -= 1
-                if candyForYou == 0:
-                    candyForYou = 1
-                res += candyForYou
-        return res
-# @lc code=end
+                candyBox[i] = candyBox[i - 1] + 1
 
+        for i in range(len(ratings) - 2, -1, -1):
+            print(i)
+            if ratings[i] > ratings[i + 1]:
+                candyBox[i] = max(candyBox[i], candyBox[i+1] + 1)
+                
+        return sum(candyBox)
+
+# @lc code=end
