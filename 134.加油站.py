bb38b23969ecbd4332b1e8bc65f3cd1c8d9655b8
mingchen <traveller31415@gmail.com>
2023-07-24 13:21:20 +0800

    贪心算法

--- 134.加油站.py
+++ 134.加油站.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        
+        startIndex, currentIndex = 0, 0
+        gasTank = 0
+        siteNum = len(gas)
+        while startIndex < siteNum and (currentIndex - startIndex < siteNum):
+            gasTank = gasTank + gas[currentIndex % siteNum] - cost[currentIndex % siteNum]
+            currentIndex += 1
+            if gasTank < 0:
+                startIndex, gasTank = currentIndex, 0
+        if startIndex >= siteNum:
+            return -1
+        else:
+            return startIndex
 # @lc code=end
 
