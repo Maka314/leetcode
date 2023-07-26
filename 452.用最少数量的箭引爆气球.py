1fd449677caf75ddc324b423e222a992f9a29e76
mingchen <traveller31415@gmail.com>
2023-07-26 09:24:02 +0800

    引爆气球题解

--- 452.用最少数量的箭引爆气球.py
+++ 452.用最少数量的箭引爆气球.py
@@ -7,21 +7,14 @@
 # @lc code=start
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        if len(points) == 1:
-            return 1
-        points.sort(key = lambda x : (x[0], x[1]))
-        res = 1
-        shotP = min(points[0][1], points[1][1])
-        for i,ball in enumerate(points[1:]):
-            if shotP >= ball[0] and shotP <= ball[1]:
-                pass #命中
+        if len(points) == 0: return 0
+        points.sort(key=lambda x: x[0])
+        result = 1
+        for i in range(1, len(points)):
+            if points[i][0] > points[i - 1][1]: # 气球i和气球i-1不挨着，注意这里不是>=
+                result += 1     
             else:
-                #未命中
-                res+=1
-                if i < len(points)-1:
-                    shotP = min(ball[1], points[i+1][1])
-                else:
-                    shotP = ball[1]
-        return res
+                points[i][1] = min(points[i - 1][1], points[i][1]) # 更新重叠气球最小右边界
+        return result
 # @lc code=end
 
