7e00963e34f8d224188ea5057b7e330b5ca19c17
mingchen <traveller31415@gmail.com>
2023-07-26 10:12:12 +0800

    线段树解

--- 2569.更新数组后处理求和查询.py
+++ 2569.更新数组后处理求和查询.py
@@ -6,32 +6,82 @@
 
 
 # @lc code=start
+class Node:
+    def __init__(self):
+        self.l = self.r = 0
+        self.s = self.lazy = 0
+
+
+class SegmentTree:
+    def __init__(self, nums):
+        self.nums = nums
+        n = len(nums)
+        self.tr = [Node() for _ in range(n << 2)]
+        self.build(1, 1, n)
+
+    def build(self, u, l, r):
+        self.tr[u].l, self.tr[u].r = l, r
+        if l == r:
+            self.tr[u].s = self.nums[l - 1]
+            return
+        mid = (l + r) >> 1
+        self.build(u << 1, l, mid)
+        self.build(u << 1 | 1, mid + 1, r)
+        self.pushup(u)
+
+    def modify(self, u, l, r):
+        if self.tr[u].l >= l and self.tr[u].r <= r:
+            self.tr[u].lazy ^= 1
+            self.tr[u].s = self.tr[u].r - self.tr[u].l + 1 - self.tr[u].s
+            return
+        self.pushdown(u)
+        mid = (self.tr[u].l + self.tr[u].r) >> 1
+        if l <= mid:
+            self.modify(u << 1, l, r)
+        if r > mid:
+            self.modify(u << 1 | 1, l, r)
+        self.pushup(u)
+
+    def query(self, u, l, r):
+        if self.tr[u].l >= l and self.tr[u].r <= r:
+            return self.tr[u].s
+        self.pushdown(u)
+        mid = (self.tr[u].l + self.tr[u].r) >> 1
+        res = 0
+        if l <= mid:
+            res += self.query(u << 1, l, r)
+        if r > mid:
+            res += self.query(u << 1 | 1, l, r)
+        return res
+
+    def pushup(self, u):
+        self.tr[u].s = self.tr[u << 1].s + self.tr[u << 1 | 1].s
+
+    def pushdown(self, u):
+        if self.tr[u].lazy:
+            mid = (self.tr[u].l + self.tr[u].r) >> 1
+            self.tr[u << 1].s = mid - self.tr[u].l + 1 - self.tr[u << 1].s
+            self.tr[u << 1].lazy ^= 1
+            self.tr[u << 1 | 1].s = self.tr[u].r - mid - self.tr[u << 1 | 1].s
+            self.tr[u << 1 | 1].lazy ^= 1
+            self.tr[u].lazy ^= 1
+
+
 class Solution:
     def handleQuery(
         self, nums1: List[int], nums2: List[int], queries: List[List[int]]
     ) -> List[int]:
-        res = []
-        for operation in queries:
-            if operation[0] == 1:
-                self.operation1(nums1, operation[1], operation[2])
-                print(1, nums1)
-            elif operation[0] == 2:
-                self.operation2(nums1, nums2, operation[1])
-                print(2, nums2)
-            elif operation[0] == 3:
-                res.append(sum(nums2))
-        return res
-
-    def operation1(self, nums, l, r):
-        for i in range(l, r + 1):
-            if not nums[i]:
-                nums[i] = 1
-            elif nums[i] == 1:
-                nums[i] = 0
-
-    def operation2(self, nums1, nums2, p):
-        for i in range(len(nums1)):
-            nums2[i] += nums1[i] * p
+        tree = SegmentTree(nums1)
+        s = sum(nums2)
+        ans = []
+        for op, a, b in queries:
+            if op == 1:
+                tree.modify(1, a + 1, b + 1)
+            elif op == 2:
+                s += a * tree.query(1, 1, len(nums1))
+            else:
+                ans.append(s)
+        return ans
 
 
 # @lc code=end
