0df4b99e7bb3c25e2012a037b8de99d94cdbfcf7
mingchen <traveller31415@gmail.com>
2023-07-26 13:24:00 +0800

    单调递增，自己写出的解

--- 738.单调递增的数字.py
+++ 738.单调递增的数字.py
@@ -7,21 +7,19 @@
 # @lc code=start
 class Solution:
     def monotoneIncreasingDigits(self, N: int) -> int:
-        strNum = str(N)
-
-        flag = len(strNum)
-        
-        for i in range(len(strNum) - 1, 0, -1):
-            if strNum[i - 1] > strNum[i]:
-                flag = i  # 更新flag的值，记录需要修改的位置
-                # 将前一个字符减1，以保证递增性质
-                strNum = strNum[:i - 1] + str(int(strNum[i - 1]) - 1) + strNum[i:]
-        
-        # 将flag位置及之后的字符都修改为9，以保证最大的递增数字
-        for i in range(flag, len(strNum)):
-            strNum = strNum[:i] + '9' + strNum[i + 1:]
-        
-        # 将最终的字符串转换回整数并返回
-        return int(strNum)
+        N = list(str(N))
+        size = len(N)
+        p = len(N)
+        for i in range(size-1, 0, -1):
+            if N[i] < N[i - 1]:
+                N[i - 1] = str(int(N[i - 1])-1)
+                p = i
+        res = 0
+        for i in range(len(N)):
+            if i < p:
+                res = res * 10 + int(N[i])
+            else:
+                res = res * 10 + 9
+        return res
 # @lc code=end
 
