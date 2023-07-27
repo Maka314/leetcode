ec9862c2ddf515e2534c351be17573f747c89260
mingchen <traveller31415@gmail.com>
2023-07-27 15:17:04 +0800

    分割问题二维DP版

--- 416.分割等和子集.py
+++ 416.分割等和子集.py
@@ -7,14 +7,28 @@
 # @lc code=start
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 != 0:
+        
+        total_sum = sum(nums)
+
+        if total_sum % 2 != 0:
             return False
-        target = sum(nums) // 2
-        dp = [0] * (target + 1)
-        for num in nums:
-            for j in range(target, num-1, -1): #这是什么意思？
-                dp[j] = max(dp[j], dp[j-num] + num)
+
+        target_sum = total_sum // 2
+        dp = [[False] * (target_sum + 1) for _ in range(len(nums) + 1)]
+
+        # 初始化第一行（空子集可以得到和为0）
+        for i in range(len(nums) + 1):
+            dp[i][0] = True
+
+        for i in range(1, len(nums) + 1):
+            for j in range(1, target_sum + 1):
+                if j < nums[i - 1]:
+                    # 当前数字大于目标和时，无法使用该数字
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    # 当前数字小于等于目标和时，可以选择使用或不使用该数字
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
         print(dp)
-        return dp[-1] == target
+        return dp[len(nums)][target_sum]
 # @lc code=end
 
