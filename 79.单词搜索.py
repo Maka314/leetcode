0c90931c6bc1682b492fc198da8c340b5587f0b1
mingchen <traveller31415@gmail.com>
2023-07-28 16:23:18 +0800

    回溯算法，控制逻辑能进回溯的进回溯

--- 79.单词搜索.py
+++ 79.单词搜索.py
@@ -8,35 +8,35 @@
 # @lc code=start
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.board = board
-        self.word = word
-        for x in range(len(self.board[0])):
-            for y in range(len(self.board)):
-                if self.board[y][x] == word[0] and self.backTracking([], x, y):
+        for x in range(len(board[0])):
+            for y in range(len(board)):
+                if self.backTracking(board, word, [], x, y):
                     return True
         return False
 
-    def backTracking(self, path, x, y):
-        path.append(self.board[y][x])
-        if len(path) == len(self.word):
+    def backTracking(self, board, word, path, x, y):
+        if x < 0 or x >= len(board[0]):
+            return False
+        if y < 0 or y >= len(board):
+            return False
+        if word[len(path)] != board[y][x]:
+            return False
+
+        path.append(board[y][x])
+        board[y][x] = "0"
+        if len(path) == len(word):
             return True
-        self.board[y][x] = 0
 
-        res = False
-        if y - 1 >= 0 and self.board[y - 1][x] == self.word[len(path)]:
-            res = self.backTracking(path, x, y - 1)
-        if (not res) and x + 1 < len(self.board[0]) and self.board[y][x + 1] == self.word[len(path)]:
-            res = self.backTracking(path, x + 1, y)
-        if (not res) and y + 1 < len(self.board) and self.board[y + 1][x] == self.word[len(path)]:
-            res = self.backTracking(path, x, y + 1)
-        if (not res) and (x - 1 >=0) and (self.board[y][x - 1] == self.word[len(path)]):
-            res = self.backTracking(path, x - 1, y)
+        if (
+            self.backTracking(board, word, path, x, y + 1)
+            or self.backTracking(board, word, path, x + 1, y)
+            or self.backTracking(board, word, path, x, y - 1)
+            or self.backTracking(board, word, path, x - 1, y)
+        ):
+            return True
 
-        if not res:
-            self.board[y][x] = path[-1]
-            path.pop
-        
-        return res
+        board[y][x] = path.pop()
+        return False
 
 
 # @lc code=end
