7443b096bf03b28550175414f5ed08244988ab20
mingchen <traveller31415@gmail.com>
2023-07-29 15:38:32 +0800

    动态规划-第一步

--- 416.分割等和子集.py
+++ 416.分割等和子集.py
@@ -7,28 +7,28 @@
 # @lc code=start
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        
-        total_sum = sum(nums)
-
-        if total_sum % 2 != 0:
+        s = sum(nums)
+        n = len(nums)
+        if s % 2:
             return False
+        
+        #需要构建一个相当于一半容量的背包
+        bag = [[0 for _ in range(s//2 + 1)] for _ in range(n)]
 
-        target_sum = total_sum // 2
-        dp = [[False] * (target_sum + 1) for _ in range(len(nums) + 1)]
-
-        # 初始化第一行（空子集可以得到和为0）
-        for i in range(len(nums) + 1):
-            dp[i][0] = True
-
-        for i in range(1, len(nums) + 1):
-            for j in range(1, target_sum + 1):
-                if j < nums[i - 1]:
-                    # 当前数字大于目标和时，无法使用该数字
-                    dp[i][j] = dp[i - 1][j]
+        #初始化第一行
+        item = 0
+        for compasity in range(s//2 + 1):
+            if compasity >= nums[item]:
+                bag[item][compasity] = nums[item]
+        
+        for item in range(1, len(nums)):
+            for compasity in range(s//2 + 1):
+                if compasity - nums[item] < 0:
+                    #不能塞下这个和其他物品
+                    bag[item][compasity] = bag[item - 1][compasity]
                 else:
-                    # 当前数字小于等于目标和时，可以选择使用或不使用该数字
-                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
-        print(dp)
-        return dp[len(nums)][target_sum]
+                    bag[item][compasity] = max(bag[item - 1][compasity], bag[item - 1][compasity - nums[item]] + nums[item], 0)
+
+        return s//2 == bag[len(nums)-1][s//2]
 # @lc code=end
 
