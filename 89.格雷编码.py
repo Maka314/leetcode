1549dc4ad5b9f81f2bf8e431f87571f93432be6b
mingchen <traveller31415@gmail.com>
2023-08-04 13:21:26 +0800

    递归法解决

---
+++ 89.格雷编码.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=89 lang=python3
+#
+# [89] 格雷编码
+#
+
+# @lc code=start
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        self.res = {1:[0, 1]}
+        return self.construct(n)
+
+    def construct(self, n):
+        if n in self.res:
+            return self.res[n]
+        
+        lastRes = self.construct(n-1)
+        res = lastRes + [i+2**(n-1) for i in lastRes[::-1]]
+        self.res[n] = res
+        return res
+# @lc code=end
+
