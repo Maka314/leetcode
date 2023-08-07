0ff4539899662ccea4961095b4d84db43edc947e
mingchen <traveller31415@gmail.com>
2023-08-07 10:51:29 +0800

    每日一题更新

--- 344.反转字符串.py
+++ 344.反转字符串.py
@@ -4,14 +4,14 @@
 # [344] 反转字符串
 #
 
+
 # @lc code=start
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        size = len(s)
-        for i in range(size//2):
+        stepCount = len(s) // 2
+        for i in range(stepCount):
             s[i], s[-(i+1)] = s[-(i+1)], s[i]
 # @lc code=end
-
