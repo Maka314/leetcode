4d47f7282ecda91f49ca621996e35bdd2deaa47f
mingchen <traveller31415@gmail.com>
2023-08-29 14:25:01 +0800

    dp?

--- 91.解码方法.py
+++ 91.解码方法.py
@@ -4,9 +4,28 @@
 # [91] 解码方法
 #
 
+
 # @lc code=start
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
-# @lc code=end
+        res = [0 for _ in range(len(s) + 1)]
 
+        res[0] = 1
+
+        # 第一位特殊判断
+        if int(s[0]) >= 1 and int(s[0]) <= 9:
+            res[1] = 1
+
+        # 第二位开始循环
+        for i in range(1, len(s)):
+            oneDigit = int(s[i])
+            twoDigit = int(s[i - 1 : i + 1])
+            if oneDigit >= 1 and oneDigit <= 9:
+                res[i + 1] += res[i]
+            if twoDigit >= 10 and twoDigit <= 26:
+                res[i + 1] += res[i - 1]
+
+        return res[-1]
+
+
+# @lc code=end
