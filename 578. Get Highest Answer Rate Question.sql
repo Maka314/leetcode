345f82b6bcdcf41a9d36bda129407bdd04e745e0
Mingchen Ju <traveller31415@gmail.com>
2023-10-08 20:50:03 +1100

    sql update

--- 578. Get Highest Answer Rate Question.sql
+++ 578. Get Highest Answer Rate Question.sql
@@ -1,53 +1,98 @@
+-- not a really good slution
 SELECT
-    MIN(survey_log) AS survey_log
+    *
 FROM
-    (SELECT
-        ShowCount.question_id AS survey_log,
-        AnswerCount.c / ShowCount.c AS Rate
-    FROM
-            (SELECT
-                question_id,
-                COUNT(*) AS c
-            FROM
-                SurveyLog
-            WHERE
-                action = 'show'
-            GROUP BY
-                question_id) AS ShowCount
-        LEFT JOIN
-            (SELECT
-                question_id,
-                COUNT(*) AS c
-            FROM
-                SurveyLog
-            WHERE
-                action = 'answer'
-            GROUP BY
-                question_id) AS AnswerCount
-        ON ShowCount.question_id = AnswerCount.question_id) AS temp1
+    (
+        SELECT
+            MIN(survey_log) AS survey_log
+        FROM
+            (
+                SELECT
+                    ShowCount.question_id AS survey_log,
+                    AnswerCount.c / ShowCount.c AS Rate
+                FROM
+                    (
+                        SELECT
+                            question_id,
+                            COUNT(*) AS c
+                        FROM
+                            SurveyLog
+                        WHERE
+                            action = 'show'
+                        GROUP BY
+                            question_id
+                    ) AS ShowCount
+                    LEFT JOIN (
+                        SELECT
+                            question_id,
+                            COUNT(*) AS c
+                        FROM
+                            SurveyLog
+                        WHERE
+                            action = 'answer'
+                        GROUP BY
+                            question_id
+                    ) AS AnswerCount ON ShowCount.question_id = AnswerCount.question_id
+            ) AS temp1
+        WHERE
+            Rate = (
+                SELECT
+                    MAX(AnswerCount.c / ShowCount.c)
+                FROM
+                    (
+                        SELECT
+                            question_id,
+                            COUNT(*) AS c
+                        FROM
+                            SurveyLog
+                        WHERE
+                            action = 'show'
+                        GROUP BY
+                            question_id
+                    ) AS ShowCount
+                    LEFT JOIN (
+                        SELECT
+                            question_id,
+                            COUNT(*) AS c
+                        FROM
+                            SurveyLog
+                        WHERE
+                            action = 'answer'
+                        GROUP BY
+                            question_id
+                    ) AS AnswerCount ON ShowCount.question_id = AnswerCount.question_id
+            )
+    ) AS Res
 WHERE
-    survey_log is not null
-    and
-    Rate = (SELECT
-                MAX(AnswerCount.c / ShowCount.c)
-            FROM
-                    (SELECT
-                        question_id,
-                        COUNT(*) AS c
-                    FROM
-                        SurveyLog
-                    WHERE
-                        action = 'show'
-                    GROUP BY
-                        question_id) AS ShowCount
-                LEFT JOIN
-                    (SELECT
-                        question_id,
-                        COUNT(*) AS c
-                    FROM
-                        SurveyLog
-                    WHERE
-                        action = 'answer'
-                    GROUP BY
-                        question_id) AS AnswerCount
-                ON ShowCount.question_id = AnswerCount.question_id)
+    survey_log IS NOT NULL;
+
+-- batter slution with sort and limit
+select
+    AnswerCnt.question_id as survey_log
+from
+    (
+        select
+            question_id,
+            count(*) as answer_cnt
+        from
+            survey_log
+        where
+            action = "answer"
+        group by
+            question_id
+    ) as AnswerCnt
+    join (
+        select
+            question_id,
+            count(*) as action_cnt
+        from
+            survey_log
+        where
+            action = "show"
+        group by
+            question_id
+    ) as ShowCnt on AnswerCnt.question_id = ShowCnt.question_id
+order by
+    AnswerCnt.answer_cnt / ShowCnt.action_cnt desc
+limit
+    1;
