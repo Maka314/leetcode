c20bd2ab232480b0a1287a2e179a259ca91ce1ab
Mingchen Ju <traveller31415@gmail.com>
2023-11-15 15:52:46 +1100

    Function conversion

---
+++ 2623. Memoize.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let memory = {}
+    return function(...args) {
+        if (!(args in memory)){
+            memory[args] = fn(...args)
+        }
+
+        return memory[args]
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
